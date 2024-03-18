8113cc4aca234b042997a408c25edd1a4a98014a
jixatos <j.gody2004@gmail.com>
2024-03-18 17:12:18 -0300

    Método getAll e Post criado com êxito

---
+++ src/main/java/br/com/fiap/springpfentregas/resource/PassageiroResource.java
@@ -0,0 +1,28 @@
+package br.com.fiap.springpfentregas.resource;
+
+import br.com.fiap.springpfentregas.entity.Passageiro;
+import br.com.fiap.springpfentregas.repository.PassageiroRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/passageiro")
+public class PassageiroResource {
+
+    @Autowired
+    private PassageiroRepository passageiroRepo;
+
+    @GetMapping
+    public List<Passageiro> getAll() {
+        return passageiroRepo.findAll();
+    }
+
+    @Transactional
+    @PostMapping
+    public Passageiro post(@RequestBody Passageiro passageiro) {
+        return passageiroRepo.save(passageiro);
+    }
+}
